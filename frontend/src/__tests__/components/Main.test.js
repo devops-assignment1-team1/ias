89dc6a9c4092f0cc820764575d430add0a8c46f2
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-16 20:11:05 +0800

    Add test for segments

--- frontend/src/__tests__/components/Main.test.js
+++ frontend/src/__tests__/components/Main.test.js
@@ -1,8 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Main from '../../pages/Main';
-import { Button } from 'bootstrap';
-import {BrowserRouter, MemoryRouter} from 'react-router-dom'
+import {MemoryRouter} from 'react-router-dom'
 
 test('Render title', async () => {
   // ARRANGE
@@ -11,4 +10,48 @@ test('Render title', async () => {
   const title = screen.container.querySelector('#title');
   expect(title).toHaveTextContent('Easy Student Internship Management');
   expect(title.nextElementSibling).toHaveTextContent('One-stop application to manage all internship matters.'); 
+})
+
+test('Render upload segment', async () => {
+  // ARRANGE
+  const screen = render(<MemoryRouter><Main/></MemoryRouter>);
+
+  // ASSERT
+  expect(screen.getByText('Upload current internship data in excel format for internship students and companies.')).toBeVisible();
+
+  const uploadFileButton = screen.getByRole('button',{name:'UPLOAD FILE'})
+  expect(uploadFileButton).toBeVisible()
+})
+
+test('Render match student segment', async () => {
+  // ARRANGE
+  const screen = render(<MemoryRouter><Main/></MemoryRouter>);
+
+  // ASSERT
+  expect(screen.getByText('Match students to their prospective companies and manage their status of internship.')).toBeVisible();
+
+  const uploadFileButton = screen.getByRole('button',{name:'MATCH STUDENT'})
+  expect(uploadFileButton).toBeVisible()
+})
+
+test('Render prepare emails segment', async () => {
+  // ARRANGE
+  const screen = render(<MemoryRouter><Main/></MemoryRouter>);
+
+  // ASSERT
+  expect(screen.getByText('Prepare emails for students attached to their prospective companies with their resumes.')).toBeVisible();
+
+  const uploadFileButton = screen.getByRole('button',{name:'PREPARE EMAILS'})
+  expect(uploadFileButton).toBeVisible()
+})
+
+test('Render settings segment', async () => {
+  // ARRANGE
+  const screen = render(<MemoryRouter><Main/></MemoryRouter>);
+
+  // ASSERT
+  expect(screen.getByText('Make changes to the file directories of emails , resumes and semester date range.')).toBeVisible();
+
+  const uploadFileButton = screen.getByRole('button',{name:'SETTINGS'})
+  expect(uploadFileButton).toBeVisible()
 })
