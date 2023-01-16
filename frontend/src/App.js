28ccbacc4d9c8ce0c76f3c1e85dddc2a5551fefc
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-16 19:23:49 +0800

    Add footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import './App.css';
+
+// Import components
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
-       
+       {/* Footer */}
+       <Footer />
     </div>
   );
 }
