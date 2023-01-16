2b1e5b990ece5696441048dce7e2eb1c12c11bbc
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-16 20:08:19 +0800

    Add title component

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+// Import components
+import { Title } from '../components/Title/Title'
+import "../components/Main/Main.css"
+
 function Main() {
   return (
-    <div></div>
+    <div style={{paddingTop:"180px",textAlign:"initial"}} className="container-fluid m-0 bg">
+    <div className="row content" style={{paddingLeft:"50px", paddingBottom:"80px"}}>
+      {/* Title */}
+      <div className="col">
+          <Title>Easy Student Internship <br/> Management</Title>
+          <p>One-stop application to manage all internship matters.</p>
+      </div>
+    </div>
+  </div>
   )
 }
 
