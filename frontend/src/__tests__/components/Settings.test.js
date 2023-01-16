13aa2eb3d72b9700ad90d99e8075a2142448a8a9
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-16 20:56:38 +0800

    Add test save changes button render

--- frontend/src/__tests__/components/Settings.test.js
+++ frontend/src/__tests__/components/Settings.test.js
@@ -12,3 +12,14 @@ test('Render title', async () => {
   expect(title).toHaveTextContent('Settings');
   expect(title.nextElementSibling).toHaveTextContent('Make changes to file directories of emails and resumes.');
 })
+
+test('Render save changes button', async () => {
+  // ARRANGE
+  const screen = render(<Settings />);
+
+  // ASSERT
+  const saveBtn = screen.container.querySelector('#save-btn');
+  expect(saveBtn).toHaveTextContent('SAVE CHANGES');
+})
+
+// //TODO:: Functional tests
