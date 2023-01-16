5e816462b148f27615ca702b3b6e664fd1d3d43a
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-16 20:33:13 +0800

    Add settings title component (not done)

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PORT from "../conn";
+
+// Import components
+import { Title } from '../components/Title/Title';
+import Button from 'react-bootstrap/Button';
+// Import style
+import "../components/Settings/Settings.css"
 
 function Settings() {
     return (
-       <div></div>
+        <div style={{paddingTop:"80px",paddingLeft:"50px",textAlign:"initial"}} className="container-fluid m-0">
+            <div className="row">
+                {/* Title */}
+                <div className="col">
+                    <Title>Settings</Title>
+                    <p>Make changes to file directories of emails and resumes.</p>
+                </div>
+            </div>
+            <hr />
+        </div>
     )
 }
 
