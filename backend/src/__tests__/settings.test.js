0bf5c80c9729e4f592340b62f244f2072b47b46a
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-17 20:45:39 +0800

    Add test for post settings

--- backend/src/__tests__/settings.test.js
+++ backend/src/__tests__/settings.test.js
@@ -1,11 +1,22 @@
 const supertest = require('supertest');
 const app = require("../server.js");
-const request = supertest(app)
+const request = supertest(app);
 
 describe('settings test suite', () => {
     test('tests get /settings endpoints', async() => {
         const response = await request.get("/api/v1/settings");
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveLength(3);
+    });
+    
+    test('tests post /settings endpoints', async() => {
+        const payload = {email_dir: 'qwerty', resume_dir: 'qwerty', internship_period: '12345' };
+        const res = await request
+                    .post('/api/v1/settings')
+                    .send(payload)
+                    .set('Content-Type', 'application/json')
+                    .set('Accept', 'application/json')
+
+        expect(res.statusCode).toBe(200);
     });
 });
