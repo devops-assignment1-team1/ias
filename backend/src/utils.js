4118acccf17b5bbff742cb6ed93afb39d4530efd
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-17 20:45:01 +0800

    Update exports

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,10 +1,10 @@
 const returnError = function returnError(res, missingVariables) {
-    res.status(400);
+    res.status(400)
     res.send(missingVariables);
 }
 
 const returnSuccess = function returnSuccess(res, data) {
-    res.status(200);
+    res.status(200)
     if (data) {
         res.send(data);
     } else {
@@ -12,5 +12,4 @@ const returnSuccess = function returnSuccess(res, data) {
     }
 }
 
-module.exports = returnError;
-module.exports = returnSuccess;
+module.exports = {returnError , returnSuccess};
