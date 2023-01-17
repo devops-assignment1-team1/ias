4527decd499c36d3ce95554c145275656d9a8425
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-17 20:07:44 +0800

    Add test for get settings

---
+++ backend/src/__tests__/settings.test.js
@@ -0,0 +1,10 @@
+const supertest = require('supertest');
+const app = require("../server.js");
+const request = supertest(app)
+
+describe('settings test suite', () => {
+    test('tests get /settings endpoints', async() => {
+        const response = await request.get("/api/v1/settings");
+        expect(response.statusCode).toBe(200);
+    });
+});
