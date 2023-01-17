be2973033ffd21f7717e96d5875ee48d8205c6f9
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-17 18:34:57 +0800

    Add render email, resume, internship upload tests
    Co-authored-by: chuadongen <chuadongen@users.noreply.github.com>

--- frontend/src/__tests__/components/Settings.test.js
+++ frontend/src/__tests__/components/Settings.test.js
@@ -47,5 +47,43 @@ test('Render email directory header', async () => {
     const internshipHeader = screen.container.querySelector('#internship-header');
     expect(internshipHeader).toHaveTextContent('Internship Period');
   })
+
+  // =================================================================
+
+  test('Render email & resume directory fields', async () => {
+    // ARRANGE
+    const screen = render(<Settings />);
   
+    // ASSERT
+    const fileDirectoryFields = screen.getAllByText('File Directory')
+    expect(fileDirectoryFields.length).toBe(2)
+  })
+  
+  test('Render update directory buttons', async () => {
+    // ARRANGE
+    const screen = render(<Settings />);
+  
+    // ASSERT
+    const updateDirectoryButtons = screen.getAllByText('UPDATE DIRECTORY')
+    expect(updateDirectoryButtons.length).toBe(2)
+  })
+
+  test('Render internship period field', async () => {
+    // ARRANGE
+    const screen = render(<Settings />);
+  
+    // ASSERT
+    const internshipPeriodField = screen.getByText('DD/MM/YYYY - DD/MM/YYYY')
+    expect(internshipPeriodField).toBeVisible()
+  })
+  
+  test('Render update period button', async () => {
+    // ARRANGE
+    const screen = render(<Settings />);
+  
+    // ASSERT
+    const updatePeriodButton = screen.getByRole('button',{name:'UPDATE PERIOD'})
+    expect(updatePeriodButton).toBeVisible()
+  })
+
 // //TODO:: Functional tests
