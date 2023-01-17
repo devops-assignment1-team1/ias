65c4828e7d9c07afc8b1a9ebfeaa6704f402f46e
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-17 20:19:54 +0800

    Add check length test

--- backend/src/__tests__/settings.test.js
+++ backend/src/__tests__/settings.test.js
@@ -6,5 +6,6 @@ describe('settings test suite', () => {
     test('tests get /settings endpoints', async() => {
         const response = await request.get("/api/v1/settings");
         expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveLength(3);
     });
 });
