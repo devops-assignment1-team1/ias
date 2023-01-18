081c67518077b419a02fa7e2b68092a47558eb96
verrgod <lightsmitevernon@gmail.com>
2023-01-18 23:29:46 +0800

    Tests for internship data header, fields and buttons

--- frontend/src/__tests__/components/Upload.test.js
+++ frontend/src/__tests__/components/Upload.test.js
@@ -11,3 +11,39 @@ test('Render title', async () => {
   expect(title).toHaveTextContent('Upload Data');
   expect(title.nextElementSibling).toHaveTextContent('Upload the corresponding excel files for the current semester here.');
 })
+
+test('Render student data header', async () => {
+    // ARRANGE
+    const screen = render(<UploadData />);
+
+    // ASSERT
+    const studentDataHeader = screen.container.querySelector('#student-file-header');
+    expect(studentDataHeader).toHaveTextContent("Student's Data");
+})
+
+test('Render company data header', async () => {
+    // ARRANGE
+    const screen = render(<UploadData />);
+
+    // ASSERT
+    const companyDataHeader = screen.container.querySelector('#company-file-header');
+    expect(companyDataHeader).toHaveTextContent("Company's Data");
+})
+
+test('Render file directory fields', async () => {
+    // ARRANGE
+    const screen = render(<UploadData />);
+
+    // ASSERT
+    const fileDirectoryFields = screen.getAllByText('File Directory')
+    expect(fileDirectoryFields.length).toBe(2)
+})
+
+test('Render upload file buttons', async () => {
+    // ARRANGE
+    const screen = render(<UploadData />);
+
+    // ASSERT
+    const uploadFileButtons = screen.getAllByText('UPLOAD FILE')
+    expect(uploadFileButtons.length).toBe(2)
+})
