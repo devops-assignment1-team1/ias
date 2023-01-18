bca9ad94b0d81b5eb7279eca0a4fbd7216a8d320
verrgod <lightsmitevernon@gmail.com>
2023-01-18 23:27:32 +0800

    Init upload page title

--- frontend/src/pages/UploadData.js
+++ frontend/src/pages/UploadData.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Title } from "../components/Title/Title";
 
 function UploadData() {
-  return (
-    <div></div>
-  )
+	return (
+		<div style={{ paddingTop: "80px", paddingLeft: "50px", textAlign: "initial" }} className="container-fluid m-0">
+			<div className="row">
+				{/* Title */}
+				<div className="col">
+					<Title>Upload Data</Title>
+					<p>Upload the corresponding excel files for the current semester here.</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default UploadData
+export default UploadData;
