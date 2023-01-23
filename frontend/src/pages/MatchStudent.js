1cb73a02273be746c33065f61c62a68ff2b6711d
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-23 16:49:32 +0800

    Add code for title component

--- frontend/src/pages/MatchStudent.js
+++ frontend/src/pages/MatchStudent.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import { Title } from '../components/Title/Title'
+
 function matchStudent() {
   return (
-    <div >
+    <div style={{paddingTop:"80px",paddingLeft:"50px",textAlign:"initial"}} className="container-fluid m-0">
+        <div className="row">
+            {/* Title */}
+            <div className="col">
+                <Title>Match Students</Title>
+                <p>Match students to companies and update their status here.</p>
+            </div>
+        </div>
+        <hr />
     </div>
   )
 }
