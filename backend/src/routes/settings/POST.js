8afdc47b68b497f1e510db2d3fcfc3690f69b215
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-24 00:29:35 +0800

    Add creation of directories

--- backend/src/routes/settings/POST.js
+++ backend/src/routes/settings/POST.js
@@ -4,16 +4,23 @@ const returnSuccess = utils.returnSuccess;
 const returnError = utils.returnError;
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+const os = require("os");
+const userHomeDir = os.homedir();
 
 const POST = router.post('/', (req, res) => {
     console.log("/settings (POST)");
     con.connect(error => {
+        /* istanbul ignore next */
         if (error) throw error;
 
         const body = req.body;
         if (body.email_dir === undefined) return returnError(res, ["email_dir"]);
         if (body.resume_dir === undefined) return returnError(res, ["resume_dir"]);
         if (body.internship_period === undefined) return returnError(res, ["internship_period"]);
+        if (!body.internship_period.includes("/")) return returnError(res, ["missing /"]);
+        if (!body.internship_period.includes("-")) return returnError(res, ["missing -"]);
 
         con.query(
             ` 
@@ -27,7 +34,10 @@ const POST = router.post('/', (req, res) => {
             `,
             [body.resume_dir, body.email_dir, body.internship_period],
             function(error, results, fields){
-                if (error) throw error;
+                /* istanbul ignore next */
+                if (error) returnError(res , "Error");
+                fs.mkdirSync(path.join(userHomeDir, body.email_dir , body.internship_period.replace("-","to").split("/").join("-")), { recursive: true })
+                fs.mkdirSync(path.join(userHomeDir, body.resume_dir , body.internship_period.replace("-","to").split("/").join("-")), { recursive: true })
                 returnSuccess(res, { "result": "success" });
             }
         );
