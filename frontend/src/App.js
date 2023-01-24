fe3675bc0dd708f4bd1c20e26bf30fa3df3ceb9f
Dong En Chua <dongenchua03@gmail.com>
2023-01-24 18:42:01 +0800

    Added prettier config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,46 @@
 // Import components
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import Footer from "./components/Footer/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import Footer from './components/Footer/Footer'
 import Main from './pages/Main'
 import UploadData from './pages/UploadData'
 import MatchStudent from './pages/MatchStudent'
 import PrepareEmail from './pages/PrepareEmail'
 import Settings from './pages/Settings'
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar'
 
-// Import React Router 
-import { Routes, Route, Navigate } from 'react-router-dom';
+// Import React Router
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-          <div style={{ flex: "1 1 auto", display: "flex", flexFlow: "column", overflowY: "hidden" }}>
-            {/* NavBar */}
-            <NavBar />
+      <div
+        style={{
+          flex: '1 1 auto',
+          display: 'flex',
+          flexFlow: 'column',
+          overflowY: 'hidden'
+        }}>
+        {/* NavBar */}
+        <NavBar />
 
-            {/* Route the different pages */}
-            <Routes>
-              <Route path='/' element={<Navigate to={"/Main"} />} />
-              <Route path='*' element={<Navigate to={"/Main"} />} />
-              <Route path='/Main' element={<Main />} />
-              <Route path='/Upload_Data' element={<UploadData />} />
-              <Route path='/Match_Student' element={<MatchStudent />} />
-              <Route path='/Prepare_Email' element={<PrepareEmail />} />
-              <Route path='/Settings' element={<Settings />} />
-            </Routes>
-          </div>
+        {/* Route the different pages */}
+        <Routes>
+          <Route path="/" element={<Navigate to={'/Main'} />} />
+          <Route path="*" element={<Navigate to={'/Main'} />} />
+          <Route path="/Main" element={<Main />} />
+          <Route path="/Upload_Data" element={<UploadData />} />
+          <Route path="/Match_Student" element={<MatchStudent />} />
+          <Route path="/Prepare_Email" element={<PrepareEmail />} />
+          <Route path="/Settings" element={<Settings />} />
+        </Routes>
+      </div>
 
-       {/* Footer */}
-       <Footer />
+      {/* Footer */}
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
