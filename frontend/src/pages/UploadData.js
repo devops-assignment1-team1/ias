aec607a8f6399d4654c9221d3102c3f443681c41
verrgod <lightsmitevernon@gmail.com>
2023-01-26 13:31:08 +0800

    Changes to upload data page

--- frontend/src/pages/UploadData.js
+++ frontend/src/pages/UploadData.js
@@ -8,39 +8,42 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
 import { BiFolderOpen } from "react-icons/bi";
+import axios, { Axios } from 'axios';
+
+// Import style
+import "../components/Settings/Settings.css"
 
 function UploadData() {
   // States
   const [showStudentFile, setShowStudentFile] = useState(false); // input modal show
   const [showCompanyFile, setShowCompanyFile] = useState(false); // input modal show
+  const [isDisabled, setDisabled] = useState(true); // save changes
 
-  // Init form data to store files
-  const [studentFormData, setStudentFormData] = useState(new FormData());
-  const [companyFormData, setCompanyFormData] = useState(new FormData());
+  // Lincoln file file 
+  const [studentFile, setStudentFile] = useState(null);
+  const [companyFile, setCompanyFile] = useState(null);
 
-  // file change when user selects file
-  const handleStudentFileChange = (event, key) => {
-    studentFormData.append(key, event);
-    setStudentFormData(studentFormData);
-    setStudentFileName(event.name);
-    handleStudentSave();
+  //lincoln upload function
+  const uploadStudentFile=(e)=>{
+    e.preventDefault()
+    let studentformData =new FormData();
+    studentformData.append("student",studentFile);
+    setStudentFileName(studentFile.name)
+    axios.post(PORT + "/api/v1/students/upload",studentformData)
     handleCloseStudentFile();
-  };
-
-  const handleCompanyFileChange = (event, key) => {
-    companyFormData.append(key, event);
-    setCompanyFormData(companyFormData);
-    setCompanyFileName(event.name);
-    handleCompanySave();
+  }
+  
+  const uploadCompanyFile=(e)=>{
+    e.preventDefault()
+    let companyformData =new FormData();
+    companyformData.append("company",companyFile);
+    setCompanyFileName(companyFile.name)
+    axios.post(PORT + "/api/v1/companies/upload",companyformData)
     handleCloseCompanyFile();
-  };
+  }
 
-  const [studentFileName, setStudentFileName] = useState("File Directory");
-  const [companyFileName, setCompanyFileName] = useState("File Directory");
-
-  // file inputs
-  const studentFileInput = React.createRef();
-  const companyFileInput = React.createRef();
+  const [studentFileName, setStudentFileName] = useState("");
+  const [companyFileName, setCompanyFileName] = useState("");
 
   const [data, setData] = useState(null);
   const [internshipPeriod, setInternshipPeriod] = useState(null);
@@ -66,24 +69,11 @@ function UploadData() {
       .catch((error) => console.log(error));
   }, []);
 
-  const handleConfirmStudentFile = (event) => {
-    var input = document.getElementById("StudentUploadFile");
-    console.log(input.files[0]);
-    handleStudentFileChange(input.files[0], "student-file");
-  };
-
-  const handleConfirmCompanyFile = (event) => {
-    var input = document.getElementById("CompanyUploadFile");
-    console.log(input.files[0]);
-    handleCompanyFileChange(input.files[0], "company-file");
-  };
-
-    // Student file show/hide handler +
+  // Student file show/hide handler +
   // Student file text update
   const handleStudentClick = () => {
     setShowStudentFile(true);
     setStudentFileName("");
-    setStudentFormData(new FormData());
   };
 
   const handleCloseStudentFile = () => {
@@ -95,45 +85,11 @@ function UploadData() {
   const handleCompanyClick = () => {
     setShowCompanyFile(true);
     setCompanyFileName("");
-    setCompanyFormData(new FormData());
   };
+
   const handleCloseCompanyFile = () => {
     setShowCompanyFile(false);
   };
-
-  async function handleStudentSave() {
-    const studentRequestOptions = {
-      method: "POST",
-      body: studentFormData,
-      headers: {
-        "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryGVn59QTi5BpftPIp",
-      },
-      redirect: "follow",
-    };
-
-    // upload student data
-    fetch(PORT + "/api/v1/students/upload", studentRequestOptions)
-      .then((response) => response.text())
-      .then((result) => toast.success("Success updating student data"))
-      .catch((error) => toast.error("Failed updating student data"));
-  }
-
-  async function handleCompanySave() {
-    var companyRequestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: companyFormData,
-      redirect: "follow",
-    };
-
-    // upload company data
-    fetch(PORT + "/api/v1/companies/upload", companyRequestOptions)
-      .then((response) => response.text())
-      .then((result) => toast.success("Success updating company data"))
-      .catch((error) => toast.error("Failed updating company data"));
-  }
 
   return (
     <div
@@ -147,128 +103,137 @@ function UploadData() {
           <p>
             Upload the corresponding excel files for the current semester here.
           </p>
-          <p id='internship-header'>
-            Internship Period: {internshipPeriod ? internshipPeriod : "Not set"}
-          </p>
-        </div>
-        {/* Student's Data */}
-        <h2
-          id="student-file-header"
-          style={{
-            paddingTop: "20px",
-            fontFamily: "var(--secondary-font)",
-            fontSize: "20px",
-          }}
-        >
-          Student's Data
-        </h2>
-        <hr />
-        <div className="container-fluid m-0">
           <div className="row">
-            {/* Text */}
-            <Card body className="col-9" style={{ fontSize: "20px" }}>
-              <BiFolderOpen /> {studentFileName}
-            </Card>
-
-            {/* Button to Upload */}
-            <div className="col-2 justify-content-center align-self-center">
-              <Button
-                variant="dark"
-                style={{ padding: "15px", width: "-webkit-fill-available" }}
-                onClick={handleStudentClick}
-              >
-                UPLOAD FILE
-              </Button>
-            </div>
+            <p>Internship Period: </p>
+            <b>{internshipPeriod}</b>
           </div>
         </div>
-
-        {/* Company's Data */}
-        <h2
-          id="company-file-header"
-          style={{
-            paddingTop: "20px",
-            fontFamily: "var(--secondary-font)",
-            fontSize: "20px",
-          }}
-        >
-          Company's Data
-        </h2>
-        <hr />
-        <div className="container-fluid m-0" style={{ paddingBottom: "100px" }}>
-          <div className="row">
-            {/* Text */}
-            <Card body className="col-9" style={{ fontSize: "20px" }}>
-              <BiFolderOpen /> {companyFileName}
-            </Card>
-
-            {/* Button to Upload */}
-            <div className="col-2 justify-content-center align-self-center">
-              <Button
-                variant="dark"
-                style={{ padding: "15px", width: "-webkit-fill-available" }}
-                onClick={handleCompanyClick}
-              >
-                UPLOAD FILE
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* Student directory modal */}
-        <Modal show={showStudentFile} onHide={handleCloseStudentFile}>
-          {/* Header with close button */}
-          <Modal.Header closeButton>
-            <Modal.Title>Choose file to upload</Modal.Title>
-          </Modal.Header>
-
-          {/* Body with input */}
-          <Modal.Body>
-            <input
-              id="StudentUploadFile"
-              formEncType='multipart/form-data'
-              type="file"
-              name="student-file"
-              defaultValue={studentFileName}
-              ref={studentFileInput}
-            />
-          </Modal.Body>
-
-          {/* Confirm selection button */}
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleConfirmStudentFile}>
-              CONFIRM
-            </Button>
-          </Modal.Footer>
-        </Modal>
-
-        {/* Company directory modal */}
-        <Modal show={showCompanyFile} onHide={handleCloseCompanyFile}>
-          {/* Header with close button */}
-          <Modal.Header closeButton>
-            <Modal.Title>Choose file to upload</Modal.Title>
-          </Modal.Header>
-
-          {/* Body with input */}
-          <Modal.Body>
-            <input
-              id="CompanyUploadFile"
-              formEncType='multipart/form-data'
-              type="file"
-              name="company-file"
-              defaultValue={companyFileName}
-              ref={companyFileInput}
-            />
-          </Modal.Body>
-
-          {/* Confirm selection button */}
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleConfirmCompanyFile}>
-              CONFIRM
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
+      <hr />
+
+      {/* Student's Data */}
+      <h2
+        id="student-file-header"
+        style={{
+          paddingTop: "20px",
+          fontFamily: "var(--secondary-font)",
+          fontSize: "20px",
+        }}
+      >
+        Student's Data
+      </h2>
+      <hr />
+      <div className="container-fluid m-0">
+        <div className="row">
+          {/* Text */}
+          <Card body className="col-9" style={{ fontSize: "20px" }}>
+            <BiFolderOpen /> {studentFileName}
+          </Card>
+
+          {/* Button to Upload */}
+          <div className="col-2 justify-content-center align-self-center">
+            <Button
+              id = "student-upload"
+              variant="dark"
+              style={{ padding: "15px", width: "-webkit-fill-available" }}
+              onClick={handleStudentClick}
+            >
+              UPLOAD FILE
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Company's Data */}
+      <h2
+        id="company-file-header"
+        style={{
+          paddingTop: "20px",
+          fontFamily: "var(--secondary-font)",
+          fontSize: "20px",
+        }}
+      >
+        Company's Data
+      </h2>
+      <hr />
+      <div className="container-fluid m-0" style={{ paddingBottom: "100px" }}>
+        <div className="row">
+          {/* Text */}
+          <Card body className="col-9" style={{ fontSize: "20px" }}>
+            <BiFolderOpen /> {companyFileName}
+          </Card>
+
+          {/* Button to Upload */}
+          <div className="col-2 justify-content-center align-self-center">
+            <Button
+              id = "company-upload"
+              variant="dark"
+              style={{ padding: "15px", width: "-webkit-fill-available" }}
+              onClick={handleCompanyClick}
+            >
+              UPLOAD FILE
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Student directory modal */}
+      <Modal show={showStudentFile} onHide={handleCloseStudentFile}>
+        {/* Header with close button */}
+        <Modal.Header closeButton>
+          <Modal.Title>Choose file to upload</Modal.Title>
+        </Modal.Header>
+
+        {/* Body with input */}
+        <Modal.Body>
+          <input
+            id="StudentUploadFile"
+            formEncType='multipart/form-data; boundary=----WebKitFormBoundaryGVn59QTi5BpftPIp'
+            type="file"
+            name="student"
+            defaultValue={studentFileName}
+            onChange={(e) => {
+              setStudentFile(e.target.files[0])
+            }}
+          />
+        </Modal.Body>
+
+        {/* Confirm selection button */}
+        <Modal.Footer>
+          <Button id="studentConfirm" variant="primary" onClick={(e)=>uploadStudentFile(e)}>
+            CONFIRM
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Company directory modal */}
+      <Modal show={showCompanyFile} onHide={handleCloseCompanyFile}>
+        {/* Header with close button */}
+        <Modal.Header closeButton>
+          <Modal.Title>Choose file to upload</Modal.Title>
+        </Modal.Header>
+
+        {/* Body with input */}
+        <Modal.Body>
+          <input
+            id="CompanyUploadFile"
+            formEncType='multipart/form-data'
+            type="file"
+            name="company"
+            defaultValue={companyFileName}
+            onChange={(e) => {
+              setCompanyFile(e.target.files[0])
+            }}
+          />
+        </Modal.Body>
+
+        {/* Confirm selection button */}
+        <Modal.Footer>
+          <Button id="companyConfirm"variant="primary" onClick={(e)=>uploadCompanyFile(e)}>
+            CONFIRM
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
