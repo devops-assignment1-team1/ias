5fd42992b657d105853b929b345163787f24999f
verrgod <lightsmitevernon@gmail.com>
2023-01-26 13:19:04 +0800

    Update POST API request test for /api/v1/students/upload, add GET tests for companies and students

--- backend/src/__tests__/students.test.js
+++ backend/src/__tests__/students.test.js
@@ -1,35 +1,25 @@
 const supertest = require('supertest');
 const app = require("../server.js");
 const request = supertest(app);
-const FormData = require('form-data');
 const fs = require('fs');
+const path = require('path');
 
-describe('test students API', () => {
-
-    beforeEach(() => {
-        // create a test file
-        fs.writeFileSync('file.xlsx', 'file content');
+describe('student test suite', () => {
+    test('tests get /students endpoints', async() => {
+        const response = await request.get("/api/v1/students");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveLength(10);
     });
-
-    afterEach(() => {
-        // delete test file
-        fs.unlinkSync('file.xlsx');
+    test('test post /students', async() => {
+        const file = fs.createReadStream(path.join(__dirname ,'..', "/routes/student/internshipData/12/12/2022-12/1/2023/gaf.csv"));
+        request
+          .post("http://localhost:5222/api/v1/students/upload")
+          .attach("student", file)
+          .expect(200)
+          .end((err, res) => {
+            if (err) throw err;
+            expect(res.body.message).toBe("File uploaded successfully");
+            done();
+          });
     })
-    
-    it('should send an excel file in post request to /api/v1/students/upload', async() => {
-        // create form data object to sync to file
-        const formData = new FormData();
-
-        // read excel file
-        const file = fs.readFileSync('file.xlsx');
-        formData.append('file', file, 'file.xlsx');
-        
-        const res = await request
-                    .post('/api/v1/students/upload')
-                    .send(formData)
-                    .expect('Content-Type', /json/)
-        
-        expect(res.body.message).toEqual('Success uploading file');
-        expect(res.statusCode).toBe(200);
-    });
 });
