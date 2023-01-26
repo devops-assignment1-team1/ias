f835b0f5b4a07ea341a5d9d70f5eb8630c97c60e
verrgod <lightsmitevernon@gmail.com>
2023-01-26 16:10:48 +0800

    Add router endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,20 @@ app.get('/api/v1', (req, res) => {
     res.send('Hello World');
 })
 
+// router for students
+const studentRouter = require("./routes/students/GET")
+app.use('/api/v1/students', studentRouter)
+
+const upsertStudentsRouter = require('./routes/students/POST');
+app.use('/api/v1/students', upsertStudentsRouter);
+
+// router for companies
+const companyRouter = require('./routes/companies/GET')
+app.use('/api/v1/companies', companyRouter)
+
+const upsertCompaniesRouter = require('./routes/companies/POST');
+app.use('/api/v1/companies', upsertCompaniesRouter);
+
 // router for settings
 const settingsRouter = require('./routes/settings/GET');
 app.use('/api/v1/settings', settingsRouter);
