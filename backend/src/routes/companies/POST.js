1fa2e87a0edde78cf3c2945de010d1ad94e78fa7
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-28 16:11:02 +0800

    Add upload companies

--- backend/src/routes/companies/POST.js
+++ backend/src/routes/companies/POST.js
@@ -1,11 +1,15 @@
 const con = require("../../db");
-const returnSuccess = require("../../utils");
-const returnError = require("../../utils");
+const utils = require("../../utils");
+const returnSuccess = utils.returnSuccess;
+const returnError = utils.returnError;
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require('path');
+const os = require("os");
+const userHomeDir = os.homedir();
 const axios = require("axios");
+const Excel = require('exceljs');
 
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
@@ -31,16 +35,51 @@ getInternshipPeriod().then(period => {
 const POST = router.post("/upload", (req, res)=>{
     const filename = req.files.company.name;
     const file = req.files.company
-    const uploadPath = path.join(__dirname, 'internshipData', internshipPeriod)
+    const uploadPath = path.join(userHomeDir, 'backup/internshipData/companies', internshipPeriod.replace("-","to").split("/").join("-"))
+    
+    /* istanbul ignore next */
     if (!fs.existsSync(uploadPath)) {
         fs.mkdirSync(uploadPath, { recursive: true });
     }
     file.mv(path.join(uploadPath, filename),(err)=>{
         if(err){
+            /* istanbul ignore next */
             return res.send(err);
         }
         else {
-            res.json({ message: "Successfully uploaded files" });
+            const workbook = new Excel.Workbook();
+            workbook.xlsx.readFile(path.join(uploadPath, filename)).then(()=>{
+                let theData = [];
+                let headers = {};
+                workbook.eachSheet((ws,sheetId)=>{
+                    
+                    /* istanbul ignore next */for(let i=1;i<=ws.actualColumnCount; i++){
+                        headers[i]=ws.getRow(1).getCell(i).value;
+                    }
+
+                    /* istanbul ignore next */for(let x=2;x<=ws.actualRowCount;x++){
+                        let theRow = {};
+
+                        for(let y=1;y<=ws.actualColumnCount;y++){
+                            theRow[headers[y]] = ws.getRow(x).getCell(y).value
+                        }
+                        theData.push(theRow)
+                    }
+                })
+                if (headers[1] || headers[2] || headers[3] || headers[4]  === undefined) return returnError(res, "Missing Headers");
+
+                con.query(
+                    ` DELETE FROM company `
+                )
+
+                theData.forEach(row => {
+                    con.query(
+                        `     
+                              INSERT INTO company(company_name, job_role, company_contact, email) VALUES (? , ? , ? , ?);`,
+                        [row["Company Name"], row["Job Role"] , row["Company Contact"] , row["Email"].text])
+                });
+                return res.json({ message: "Successfully updated & uploaded files" });
+            })
         }
     })
 })
