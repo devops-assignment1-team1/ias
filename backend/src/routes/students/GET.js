f4186824b306f51e03074c5a6ebcc339a0cba5d3
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-28 16:14:37 +0800

    Add get students

--- backend/src/routes/students/GET.js
+++ backend/src/routes/students/GET.js
@@ -1,18 +1,33 @@
 const con = require("../../db");
-const returnSuccess = require("../../utils");
-const returnError = require("../../utils");
+const utils = require("../../utils");
+const returnSuccess = utils.returnSuccess;
+const returnError = utils.returnError;
 const express = require("express");
+const e = require("express");
 const router = express.Router();
 
 const GET = router.get('/', (req, res) => {
-    con.connect(error => {
-        if (error) throw error;
+    console.log("students /GET")
+    const { status } = req.query;
 
-        con.query('SELECT * FROM student', (error, results) => {
-            if (error) throw error;
-            returnSuccess(res, results);
-        })
-    })
+    con.connect(error => {
+        /* istanbul ignore next */
+        if (error) return returnError(res, "Database Error");
+
+        if (status) {
+            con.query('SELECT * FROM student WHERE status = ?', [status], (error, results) => {
+                if (error) return returnError(res, "Database Error");
+                returnSuccess(res, results);
+            });
+        } else {
+            con.query('SELECT * FROM student', (error, results) => {
+                /* istanbul ignore next */
+                if (error) return returnError(res, "Database Error");
+                returnSuccess(res, results);
+            });
+        }
+
+    });
 });
 
 module.exports = GET;
