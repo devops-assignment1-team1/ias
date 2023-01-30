e108e23f18c5e68065ca321629a5cdbb7c8fdcab
NPLeeWenKang <s10203100@connect.np.edu.sg>
2023-01-31 00:21:23 +0800

    Test pipeline 4

--- integrationTest/test_assignStudent.py
+++ integrationTest/test_assignStudent.py
@@ -1,13 +1,16 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select , WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_assign_student():
-
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
 
     driver.get("http://localhost:5221/Match_Student")
