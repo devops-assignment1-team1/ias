fbc2d078cdbb6f80e954a1ae50cac643e2b5504a
lincolnchia <s10205479@connect.np.edu.sg>
2023-02-04 14:09:40 +0800

    Testcases for matching student and preparing email
    Co-authored-by: verr <verrgod@users.noreply.github.com>

---
+++ integrationTesting/test_prepare_email_error.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.chrome.options import Options                                                                                                                          
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select , WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_prepare_email_no_resume():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=chrome_options)
+
+    driver.get("http://localhost:5221/Prepare_Email")
+
+    title = driver.title
+    assert title == "IAS"
+
+    driver.implicitly_wait(0.5)
+
+    generateEmail = driver.find_element(By.ID, "email-btn")
+    generateEmail.click()
+
+    wait = WebDriverWait(driver, 20)
+
+    toast = wait.until(EC.element_to_be_clickable((driver.find_element(By.CLASS_NAME, "go3958317564"))))
+    toast = toast.text
+
+    assert toast == "Email prepared. Missing resume(s) from Balqis"
+
+    driver.quit()
---
+++ integrationTesting/test_prepare_email_success.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select , WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import shutil
+from pathlib import Path
+
+def test_match_student_prepare_email():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=chrome_options)
+
+    driver.get("http://localhost:5221/Prepare_Email")
+
+    title = driver.title
+    assert title == "IAS"
+
+    driver.implicitly_wait(0.5)
+
+    src = os.getcwd() + "/TestData/balqis.pdf"
+
+    dist = os.path.expanduser("~") + "/eexports/resume/23-07-2023 to 23-08-2023"
+
+    shutil.copy(src, dist)
+
+    generateEmail = driver.find_element(By.ID, "email-btn")
+    generateEmail.click()
+
+    wait = WebDriverWait(driver, 20)
+
+    toast = wait.until(EC.element_to_be_clickable((driver.find_element(By.CLASS_NAME, "go3958317564"))))
+    toast = toast.text
+
+    assert toast == "Email prepared successfully"
+
+    driver.quit()
