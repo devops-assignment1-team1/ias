ce677a3d9c78e8e6a1bdd7295caa1d9251273c17
lincolnchia <s10205479@connect.np.edu.sg>
2023-02-05 16:06:46 +0800

    adding chrome settings

--- integrationTesting/test_upload_data_match_student.py
+++ integrationTesting/test_upload_data_match_student.py
@@ -12,6 +12,7 @@ def test_uploadData_MatchStudent():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--window-size=1920x1080")
     driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=chrome_options)
 
     driver.get("http://localhost:5221/Upload_Data")
