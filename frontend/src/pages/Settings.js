a559ea0a4d0cd813445eda3a24501a7f35f921dc
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-02-13 11:09:51 +0800

    Change the int to string

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -73,22 +73,23 @@ function Settings() {
     const handleClose = () => {
         setShow(false);
 
-        if(value.length === 2){
-            const start = value[0];
-            const end = value[1];
+        // if(value.length === 2){
+            // const start = value[0];
+            // const end = value[1];
             
-            // Start date
-            const dayStart = String(start.day).length === 1 ? "0" + String(start.day) : String(start.day);
-            const monthStart = String(start.month.index + 1).length === 1 ? "0" + String(start.month.index + 1) : String(start.month.index + 1);
-            const startStr = dayStart + "/" + monthStart + "/" + String(start.year);
+            // // Start date
+            // const dayStart = String(start.day).length === 1 ? "0" + String(start.day) : String(start.day);
+            // const monthStart = String(start.month.index + 1).length === 1 ? "0" + String(start.month.index + 1) : String(start.month.index + 1);
+            // const startStr = dayStart + "/" + monthStart + "/" + String(start.year);
 
-            // End date
-            const dayEnd = String(end.day).length === 1 ? "0" + String(end.day) : String(end.day);
-            const monthEnd = String(end.month.index + 1).length === 1 ? "0" + String(end.month.index + 1) : String(end.month.index + 1);
-            const endStr = dayEnd + "/" + monthEnd + "/" + String(end.year);
-            setInternshipPeriod(startStr + " - " + endStr);
+            // // End date
+            // const dayEnd = String(end.day).length === 1 ? "0" + String(end.day) : String(end.day);
+            // const monthEnd = String(end.month.index + 1).length === 1 ? "0" + String(end.month.index + 1) : String(end.month.index + 1);
+            // const endStr = dayEnd + "/" + monthEnd + "/" + String(end.year);
+            // setInternshipPeriod(startStr + " - " + endStr);
+            setInternshipPeriod(document.getElementById("int-period").value)
             setTitle("Settings - Not Saved");
-        };
+        // };
     };
 
     // Email directory show/hide handler +
@@ -234,7 +235,12 @@ function Settings() {
 
                 {/* Body with date picker */}
                 <Modal.Body>
-                    <Calendar range minDate={new Date()} numberOfMonths={2} value={value} onChange={setValue}/>
+                    {/* <Calendar range minDate={new Date()} numberOfMonths={2} value={value} onChange={setValue}/> */}
+                    <Form.Control
+                        type="text"
+                        id = "int-period"
+                        data-testid="int-period"
+                    />
                 </Modal.Body>
 
                 {/* Confirm selection button */}
